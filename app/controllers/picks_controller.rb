60e5cd715ffaf9c072b8854f494cadf1f0f16466
John Gesimondo <john@jmondo.com>
2013-04-22 21:52:11 -0700

    6 picks v0.1

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,4 +1,26 @@
 class PicksController < ApplicationController
+  EXCLUDED_CATEGORIES = ["Coffee Shop"]
+
   def index
+    @picks = fetch_picks
+  end
+
+  def fetch_picks
+    params = {
+      "intent" => "checkin",
+      "ll" => "37.791442,-122.392723",
+      "radius" => 500,
+      "categoryId" => "4d4b7105d754a06374d81259",
+      "oauth_token" => "XRSNYBSIQWGAQZ0F4PHBZLPRBQUMPMYHK5PV33U5CXH5RFUA",
+      "client_id" => ENV['FOURSQUARE_CLIENT_ID'],
+      "client_secret" => ENV['FOURSQUARE_CLIENT_SECRET'],
+      "v" => Date.today.strftime('%Y%m%d'),
+      "limit" => 50
+    }
+
+    response = HTTParty.get('https://api.foursquare.com/v2/venues/search', :query => params)
+    response = Hashie::Mash.new(response).response.venues
+    response.delete_if {|restaurant| EXCLUDED_CATEGORIES.include?(restaurant.categories.first.name)}
+    response.sample(6)
   end
 end
