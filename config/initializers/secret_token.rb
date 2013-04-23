0ada61e696b6796f66a69b75804ddd5010a0ac38
John Gesimondo <john@jmondo.com>
2013-04-22 20:08:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixpicks::Application.config.secret_token = '6cc4e0f6d7f6b80882dc05299f206989484c40afda740b676aaff9fb5f30096bb494e925767890fcb6fa4e63904ed76b06bc37e70c0871aa4773d71169fa40e1'
